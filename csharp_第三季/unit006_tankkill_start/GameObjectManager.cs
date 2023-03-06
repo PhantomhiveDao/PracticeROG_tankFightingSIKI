f48e20de9b25fd110f4ae9168e143cdeb3d3b82c
DaoZhang <1076354486@qq.com>
2023-03-06 23:00:25 +0800

    “完成了地图的绘制且绘制出了boss“

--- csharp_第三季/unit006_tankkill_start/GameObjectManager.cs
+++ csharp_第三季/unit006_tankkill_start/GameObjectManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,45 +11,98 @@ namespace unit006_tankkill_start
     class GameObjectManager
 
     {
-        private static List<NotMovething> wallLists = new List<NotMovething>();
+        private static List<NotMovething> wallList = new List<NotMovething>();
+        private static List<NotMovething> steelList = new List<NotMovething>();
+        private static NotMovething boss;
 
         public static void DrawMap()
         {
-            foreach (NotMovething nm in wallLists)
-            {
-                nm.DrawSelf();
-                //Console.WriteLine("666");
-            }
-            //for (int i = 0; i < wallList.Count; i++)
+            //foreach (NotMovething nm in wallLists)
             //{
-            //    wallList[i].DrawSelf();
+            //    nm.DrawSelf();
+            //    //Console.WriteLine("666");
             //}
-            //Console.WriteLine("drawmap end");
+            for (int i = 0; i < wallList.Count; i++)
+            {
+                wallList[i].DrawSelf();
+            }
+            Console.WriteLine("drawmap end");
+            for (int i = 0; i < steelList.Count; i++)
+            {
+                steelList[i].DrawSelf();
+            }
+            boss.DrawSelf();
+            Console.WriteLine("drawmap end");
         }
 
         public static void CreatMap()
         {
-             CreateWall(1,1, 5,wallLists);
-             CreateWall(8, 4, 5,wallLists);
+            CreateWall(1, 1, 5, Resources.wall, wallList);
+            CreateWall(3, 1, 5, Resources.wall, wallList);
+            CreateWall(5, 1, 4, Resources.wall, wallList);
+            CreateWall(7, 1, 3, Resources.wall, wallList);
+            CreateWall(9, 1, 4, Resources.wall, wallList);
+            CreateWall(11, 1, 5, Resources.wall, wallList);
+            CreateWall(13, 1, 5, Resources.wall, wallList);
+
+            CreateWall(7, 5, 1, Resources.steel, steelList);
+
+            CreateWall(0, 7, 1, Resources.steel, steelList);
+
+            CreateWall(2, 7, 1, Resources.wall, wallList);
+            CreateWall(3, 7, 1, Resources.wall, wallList);
+            CreateWall(4, 7, 1, Resources.wall, wallList);
+            CreateWall(6, 7, 1, Resources.wall, wallList);
+            CreateWall(7, 6, 2, Resources.wall, wallList);
+            CreateWall(8, 7, 1, Resources.wall, wallList);
+            CreateWall(10, 7, 1, Resources.wall, wallList);
+            CreateWall(11, 7, 1, Resources.wall, wallList);
+            CreateWall(12, 7, 1, Resources.wall, wallList);
+
+            CreateWall(14, 7, 1, Resources.steel, steelList);
+
+            CreateWall(1, 9, 5, Resources.wall, wallList);
+            CreateWall(3, 9, 5, Resources.wall, wallList);
+            CreateWall(5, 9, 3, Resources.wall, wallList);
+
+            CreateWall(6, 10, 1, Resources.wall, wallList);
+            CreateWall(7, 10, 2, Resources.wall, wallList);
+            CreateWall(8, 10, 1, Resources.wall, wallList);
+
+            CreateWall(9, 9, 3, Resources.wall, wallList);
+            CreateWall(11, 9, 5, Resources.wall, wallList);
+            CreateWall(13, 9, 5, Resources.wall, wallList);
+
+
+            CreateWall(6, 13, 2, Resources.wall, wallList);
+            CreateWall(7, 13, 1, Resources.wall, wallList);
+            CreateWall(8, 13, 2, Resources.wall, wallList);
+
+            CreateBoss(7, 14, Resources.Boss);
 
         }
         //创建墙
-        private static void  CreateWall(int x,int y,int count, List<NotMovething> wallList)//x,y,从第几个点开始创建
+        private static void  CreateWall(int x,int y,int count, Image img ,List<NotMovething> wallList)//x,y,从第几个点开始创建
         {           
             int xPosition = x * 30;
             int yPosition = y * 30;
             for (int i = yPosition; i < yPosition + count * 30; i += 15)
             {
 
-                NotMovething wall1 = new NotMovething(xPosition, i, Resources.wall);
-                NotMovething wall2 = new NotMovething(xPosition + 15, i,Resources.wall);
+                NotMovething wall1 = new NotMovething(xPosition, i, img);
+                NotMovething wall2 = new NotMovething(xPosition + 15, i, img);
                 wallList.Add(wall1);
                 wallList.Add(wall2);
                 Console.WriteLine(i);
 
-            }
-           // return wallList;
-            
+            }           
+        }
+        //创建boss
+        private static void CreateBoss(int x, int y, Image img)
+        {
+            int xPosition = x * 30;
+            int yPosition = y * 30;
+            boss = new NotMovething(xPosition, yPosition, img);
         }
 
     }
